322313026de6e5e6efc7af2aa0e2eb3ddb759002
rituraj roj <riturajroj1234@gmail.com>
2025-09-04 18:04:21 +0530

    seting rating with user (mapping of ratings)

--- UserService/src/main/java/com/lcr/user/service/services/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/lcr/user/service/services/impl/UserServiceImpl.java
@@ -1,15 +1,19 @@
 package com.lcr.user.service.services.impl;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.lcr.user.service.entities.Hotel;
 import com.lcr.user.service.entities.Rating;
 import com.lcr.user.service.entities.User;
 import com.lcr.user.service.exceptions.ResourceNotFoundException;
@@ -50,13 +54,29 @@ public class UserServiceImpl implements UserService {
     
                 // http://localhost:8083/ratings/users/0d5bdb25-2b02-488b-a48b-1e48bb3066ab
 
-        @SuppressWarnings("unchecked")
-        ArrayList<Rating> ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/"+user.getUserId(), ArrayList.class);
+        // @SuppressWarnings("unchecked")
+        Rating[] ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/"+user.getUserId(), Rating[].class);
         
         logger.info("{}",ratingsOfUser);
 
+       List<Rating> ratings =  Arrays.stream(ratingsOfUser).toList();
+
+        List<Rating> ratingList = ratings.stream().map(rating->{
+
+            // http://localhost:8082/hotels/af0ab699-a0a7-48a6-afd7-290f394b81d1
+            ResponseEntity<Hotel> forEntity = restTemplate.getForEntity("http://localhost:8082/hotels/"+rating.getHotelId(), Hotel.class);
+            Hotel hotel = forEntity.getBody();
+
+            // set the hotel rating
+            rating.setHotel(hotel);
+
+            return rating;
+            
+        }).collect(Collectors.toList());
+
+
         // setting rating into user , so when we fire user , we also get the ratings provided by tjat user
-        user.setRatings(ratingsOfUser);
+        user.setRatings(ratingList);
 
         return user;
     }
