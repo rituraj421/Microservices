1ac48ec956d51a278cda51aee16d56faeddc7275
rituraj roj <riturajroj1234@gmail.com>
2025-09-04 21:02:38 +0530

    feign client updation

--- UserService/src/main/java/com/lcr/user/service/UserServiceApplication.java
+++ UserService/src/main/java/com/lcr/user/service/UserServiceApplication.java
@@ -5,19 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 // import org.springframework.context.annotation.Bean;
 // import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 // now adding annotation for eureka (this is also optional, as we have already added the dependency in pom file)
 @EnableDiscoveryClient
+@EnableFeignClients
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
-
-		// creating bean for microservice
-		// @Bean
-		// public RestTemplate restTemplate(){
-		// 	return new RestTemplate();
-		// }
 
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
