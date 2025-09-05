3aef9f3afbf914b011e2bdff48f150881cf02881
rituraj roj <riturajroj1234@gmail.com>
2025-09-05 14:59:48 +0530

    implemented feign client test

--- UserService/src/test/java/com/lcr/user/service/UserServiceApplicationTests.java
+++ UserService/src/test/java/com/lcr/user/service/UserServiceApplicationTests.java
@@ -1,13 +1,28 @@
 package com.lcr.user.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.lcr.user.service.entities.Rating;
+import com.lcr.user.service.external.services.RatingService;
 
 @SpringBootTest
 class UserServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+    @Autowired
+	private RatingService ratingService;
+
+	@Test
+	void createRating(){
+		Rating rating = Rating.builder().rating("10").userId("null").hotelId(" ").feedback("this is created using feign client").build();
+		Rating savedRating = ratingService.createRating(rating);
+
+		System.out.println("NEW RATING CREATED");
 	}
 
 }
